91b7b77d8f5c2b567054b0b64a8806e5511bfea7
Danny Robinson <dannyrobinson@gmx.com>
2018-01-20 10:46:22 +0000

    Prettified the code, added ditherkitty to the readme, MIT licensed

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 My implementation of [Floyd-Steinberg Dithering](https://en.wikipedia.org/wiki/Floyd–Steinberg_dithering) from [Coding Train's](https://github.com/CodingTrain) [Coding Challenge 90.](https://www.youtube.com/watch?v=0L2n8Tg2FwI)
 
-[Live version](https://stellartux.github.io/CC90/).
+[Live version](https://stellartux.github.io/CC90/)
+
+![Dither Kitty](/assets/ditherkitty.png)
+
+**D̡̺͖̘͔̿̋Î̥͍̱͡Tͪ̍͆͏̥͔̩͇͇H͌͠Ȇ̌̉ͯ̿͊́R̳̦̖̱̟͒ͫ͐̅ ̖͇̭͇͎̖͊͐̽ͪ̀K̢͓̯͕̫̠̰̯̀̋͐͛̋͌I̻̐̄̋͝T̖̙͈̞̦͖̓ͥͤ̏̒͝ͅT̑̄̅Y̧͕̮̯͎͛́ͅ ̙̞̖̖͎̳T̺̜͋͆̏ͪ̊̈̉H͍͎̳͙͙͑͑͛ͩR͌͆ͣ̄̿ͩ͏I̥̲̦̤̺͛ͮ̾ͣV̘̻ͬͤ͊̒ͧ͡Ẽͬ͗ͭ̂ͦ͏͈S̟ͥ͑̿ ̑͑̈́͏̰͍̺̺͖͍̰I̤̗̹ͨͥ̂̓̑ͥǸ ̶̲̰͚͖̺̯̮̋͑C̲̫̗̳͉̺̋̓̔̾ͬͧ͂H͟Ǎ̶͕O̸̳̪ͤ́̆ͩS̢̞̫͔͚͎̼͓s**
--- sketch.js
+++ sketch.js
@@ -1,48 +1,46 @@
-
-
 let inputFactor, inputUserPic, userFile, userImage, resetButton, ditherSelect;
 
 function setup() {
-  title = createElement('h2', 'Dither Kitty!');
+  createElement("h2", "Dither Kitty!");
 
   createCanvas(100, 100);
 
-  createSpan('Dither type: ');
+  createSpan("Dither type: ");
   ditherSelect = createSelect();
-  ditherSelect.option('None');
-  ditherSelect.option('Simple');
-  ditherSelect.option('Floyd-Steinberg');
+  ditherSelect.option("None");
+  ditherSelect.option("Simple");
+  ditherSelect.option("Floyd-Steinberg");
   ditherSelect.changed(handleChange);
 
-  createSpan('Dither amount: ');
+  createSpan("Dither amount: ");
   inputFactor = createSlider(1, 510, 255, 1);
   inputFactor.changed(handleChange);
 
-  createSpan('Upload an image: ')
+  createSpan("Upload an image: ");
   inputUserPic = createFileInput(handleNewImage);
-  inputUserPic.elt.accept = 'image/*';
+  inputUserPic.elt.accept = "image/*";
 
-  createSpan('Get the kitten back: ')
-  resetButton = createButton('Reset');
+  createSpan("Get the kitten back: ");
+  resetButton = createButton("Reset");
   resetButton.mouseClicked(reset);
 
-  createA('https://github.com/stellartux/CC90', 'Source Code');
+  createA("https://github.com/stellartux/CC90", "Source Code");
 
   noLoop();
   reset();
 }
 
 function reset() {
-  userImage = loadImage('assets/kitten.jpg', display);
+  userImage = loadImage("assets/kitten.jpg", display);
   userFile = undefined;
 }
 
 function handleChange() {
-    if (userFile) {
-      handleNewImage(userFile)
-    } else {
-      reset()
-    }
+  if (userFile) {
+    handleNewImage(userFile);
+  } else {
+    reset();
+  }
 }
 
 function handleNewImage(file) {
@@ -51,11 +49,11 @@ function handleNewImage(file) {
 }
 
 function display(img) {
-  resizeCanvas(img.width, img.height)
+  resizeCanvas(img.width, img.height);
   background(0);
-  if (ditherSelect.value() === 'Simple') {
-    simpleDither(img)
-  } else if (ditherSelect.value() === 'Floyd-Steinberg') {
+  if (ditherSelect.value() === "Simple") {
+    simpleDither(img);
+  } else if (ditherSelect.value() === "Floyd-Steinberg") {
     floydSteinbergDither(img);
   }
   image(img, 0, 0);
@@ -74,7 +72,7 @@ function floydSteinbergDither(img) {
   img.loadPixels();
   img.pixels.forEach((value, index, pixels) => {
     let x = floor(index / 4) % img.width;
-    let y = floor((index / 4) / img.width);
+    let y = floor(index / 4 / img.width);
 
     pixels[index] = quantise(value);
     let error = quantError(value);
@@ -82,13 +80,12 @@ function floydSteinbergDither(img) {
       pixels[index + 4] += round(error * 7 / 16);
     }
     if (y < img.height - 1) {
-      pixels[index + (y * 4)] += round(error * 5 / 16);
+      pixels[index + y * 4] += round(error * 5 / 16);
       if (x > 0) {
-        pixels[index - 4 + (y * 4)] += round(error * 3 / 16);
+        pixels[index - 4 + y * 4] += round(error * 3 / 16);
       }
-      if (x < img.width - 1)
-      {
-        pixels[index + 4 + (y * 4)] += round(error / 16);
+      if (x < img.width - 1) {
+        pixels[index + 4 + y * 4] += round(error / 16);
       }
     }
   });
@@ -96,8 +93,8 @@ function floydSteinbergDither(img) {
 }
 
 function quantise(value, factor = inputFactor.value()) {
-  return round(value / factor) * factor
+  return round(value / factor) * factor;
 }
 function quantError(value, factor = inputFactor.value()) {
-  return (value - round(value / factor) * factor)
+  return value - round(value / factor) * factor;
 }
